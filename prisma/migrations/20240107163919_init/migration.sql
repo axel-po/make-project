0871cab99d0bc088bd89bdbe4312b531c9c65a02
Axel Po <axelpoin@gmail.com>
2024-01-07 17:45:41 +0100

    Add @faker-js/faker dependency and create Project model

--- prisma/migrations/20240107163919_init/migration.sql
+++ prisma/migrations/20240107163919_init/migration.sql
@@ -50,6 +50,16 @@ CREATE TABLE "VerificationToken" (
     "expires" DATETIME NOT NULL
 );
 
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Project_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
 -- CreateIndex
 CREATE INDEX "Post_name_idx" ON "Post"("name");
 
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,17 @@
 import Login from "@/components/Login";
 import User from "@/components/User";
-
+import { db } from "@/server/db";
 import React from "react";
 
-const HomePage = () => {
+const HomePage = async () => {
+  const data = await db.project.findMany({
+    select: {
+      title: true,
+    },
+  });
+
+  console.log("data", data);
+
   return (
     <>
       <Login />
