e949ce03f4505c7fc0b443b41d4a2cf3d37ade14
Axel Po <axelpoin@gmail.com>
2024-01-14 22:17:46 +0100

    Add Sidebar component and update language to English

---
+++ src/features/layout/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <nav className="min-h-screen w-[400px] border-r border-neutral-200 bg-red-400">
+      <p>Dashboard</p>
+    </nav>
+  );
+};
+
+export default Sidebar;
