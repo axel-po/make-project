665cdc6d0411804501f15dbd5da4a43fdf6b3c22
Axel Pointud <axelpo.tod@gmail.com>
2024-01-31 16:03:54 +0100

    Remove unused import

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { format } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
